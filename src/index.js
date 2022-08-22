6aae1a8f8341491411fcd1ee9a547b2da1161e61
Ilya Boyandin <ilya@boyandin.me>
2022-08-22 17:59:40 +0200

    Using layersRenderData

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import { createMap } from "http://localhost:8090/index.js";
-import { csvParse, bisectLeft, range } from "d3";
+import { csvParse, range } from "d3";
 import "./css/main.css";
 
 const NUM_CIRCLES = 10;
 const DATASET_ID = "5b1fd10b-cbee-42bc-b162-4536e0c9252d"; // this id is referenced in map-config.json
+const TRIP_LAYER_ID = "hzc32m9";
 
 (async () => {
   const mapConfig = await (await fetch("/data/map-config.json")).json();
   const flightData = csvParse(
     await (await fetch("/data/flight-data.csv")).text()
   );
-  const timeSteps = flightData.map((d) => +d.timestamp * 1000).sort();
-
   const map = await createMap({ container: document.querySelector("#root") });
   map.setMapConfig(mapConfig, {
     additionalDatasets: [
@@ -22,46 +21,37 @@ const DATASET_ID = "5b1fd10b-cbee-42bc-b162-4536e0c9252d"; // this id is referen
       },
     ],
   });
-  map._setDeckRenderEnhancer((deckProps, { DeckGLLayers, layerTimeline }) => {
-    const { layers } = deckProps;
-    const { currentTime } = layerTimeline;
-    const timeIndex = findClosestTimeIndex(currentTime);
-    const proximity = getProximityToTimeIndex(timeIndex, currentTime);
-    const datum = flightData[timeIndex];
-    return {
-      layers: [
-        new DeckGLLayers.ScatterplotLayer({
-          id: "my-scatterplot-layer",
-          data: datum ? range(NUM_CIRCLES) : [],
-          pickable: false,
-          opacity: 1,
-          stroked: true,
-          filled: false,
-          radiusScale: proximity,
-          radiusUnits: "pixels",
-          lineWidthMinPixels: 1,
-          getPosition: (d) => [+datum.lon, +datum.lat, +datum.altitude],
-          getRadius: (i) => 100 * (i / NUM_CIRCLES),
-          getLineColor: (i) => [
-            255,
-            0,
-            (255 * i) / NUM_CIRCLES,
-            (1.0 - proximity) * 255,
-          ],
-        }),
-        ...layers,
-      ],
-    };
-  });
-
-  function findClosestTimeIndex(currentTime) {
-    return bisectLeft(timeSteps, currentTime);
-  }
-  function getProximityToTimeIndex(timeIndex, currentTime) {
-    return timeIndex < timeSteps.length - 2
-      ? 1.0 -
-          Math.abs(currentTime - timeSteps[timeIndex]) /
-            (timeSteps[timeIndex + 1] - timeSteps[timeIndex])
-      : 0.0;
-  }
+  map._setDeckRenderCallback(
+    (
+      deckProps,
+      { mapIndex, DeckGLLayers, layerTimeline, layersRenderData }
+    ) => {
+      const { layers } = deckProps;
+      const { position, advancement } = layersRenderData[TRIP_LAYER_ID].data[0];
+      return {
+        layers: [
+          new DeckGLLayers.ScatterplotLayer({
+            id: "my-scatterplot-layer",
+            data: range(NUM_CIRCLES),
+            pickable: false,
+            opacity: 1,
+            stroked: true,
+            filled: false,
+            radiusScale: advancement,
+            radiusUnits: "pixels",
+            lineWidthMinPixels: 3,
+            getPosition: position,
+            getRadius: (i) => 100 * (i / NUM_CIRCLES),
+            getLineColor: (i) => [
+              255,
+              0,
+              (255 * i) / NUM_CIRCLES,
+              (1.0 - advancement) * 255,
+            ],
+          }),
+          ...layers,
+        ],
+      };
+    }
+  );
 })();
