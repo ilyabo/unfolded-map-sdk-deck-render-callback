075d0539ac65813937e8180b41b44658871b8b96
Chris Gervang <chris.gervang@joby.aero>
2023-01-11 15:12:41 -0800

    Add basic animation and video render

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@
 // import { createMap } from "@unfolded/map-sdk";
 import {createUnfoldedRenderTarget} from './lib/unfolded-render-target'
 import { csvParse, range } from "d3";
+import {interpolate} from 'popmotion'
 import "./css/main.css";
+import * as dat from 'dat.gui';
 
 const NUM_CIRCLES = 10;
 const DATASET_ID = "5b1fd10b-cbee-42bc-b162-4536e0c9252d"; // this id is referenced in map-config.json
 const TRIP_LAYER_ID = "hzc32m9";
+
+const controls = {
+  hideUnfolded: false,
+  durationSec: 5,
+  scrubberMs: 0
+};
+
+const gui = new dat.GUI();
 
 (async () => {
   const mapConfig = await (await fetch("/data/map-config.json")).json();
@@ -27,6 +37,58 @@ const TRIP_LAYER_ID = "hzc32m9";
       },
     ],
   });
+  unfoldedMap.setProps({hidden: controls.hideUnfolded});
+
+  setAnimation({mapSdk, durationSec: controls.durationSec, renderer})
+
+  // gui
+  const hideUnfolded = gui.add(controls, 'hideUnfolded').name('Hide Unfolded Editor').listen();
+  hideUnfolded.onChange(function (hidden) {
+    unfoldedMap.setProps({hidden});
+  });
+
+  const timeScrubber = gui.add(controls, 'scrubberMs', 0, controls.durationSec * 1000).name('Time Scrubber').listen()
+  timeScrubber.onChange(function (newScrubberMs) {
+    renderer.seek(newScrubberMs)
+  })
+
+  const durationSec = gui.add(controls, 'durationSec', 1, 30).name('Video Length').listen()
+  durationSec.onChange(function (newDurationSec) {
+    setAnimation({mapSdk, durationSec: newDurationSec, renderer})
+
+    // Sync scrubber
+    const newDurationMs = newDurationSec * 1000;
+    if (controls.scrubberMs > newDurationMs) {
+      // scrubber needs to stay within animation.
+      controls.scrubberMs = newDurationMs
+    }
+    timeScrubber.max(newDurationMs)
+    timeScrubber.setValue(controls.scrubberMs)
+  })
+
+  const hubbleLib = unfoldedMap.getLibs()?.hubble;
+
+  gui.add({render: function () {
+    hideUnfolded.setValue(true)
+
+    renderer.setProps({
+      Encoder: hubbleLib.WebMEncoder,
+      formatConfigs: {webm: {quality: 0.99}}
+    })
+    renderer.render()
+  }}, 'render').name('Render Video')
+
+  gui.add({preview: function () {
+    renderer.setProps({Encoder: hubbleLib.PreviewEncoder})
+    renderer.render()
+  }}, 'preview').name('Preview Video')
+
+  gui.add({stop: function () {
+    hideUnfolded.setValue(false)
+    renderer.stop({})
+    timeScrubber.setValue(0)
+  }}, 'stop').name('Stop Render')
+
 })();
 
 function onDeckLoad({ deck }) {
@@ -90,4 +152,25 @@ async function generateData(n = 10) {
     }
   }
   return newData;
+}
+
+function getTimecode(durationSec) {
+  return {start: 0, end: Math.floor(durationSec * 1000), framerate: 30}
+}
+
+function setAnimation({mapSdk, durationSec, renderer}) {
+  // Set when duration or data domain changes.
+  // Animation must be set after data is loaded.
+  const unfoldedTimeline = mapSdk.getLayerTimeline() 
+  const getTimestamp = interpolate(
+    [0, Math.floor(durationSec * 1000)],
+    [unfoldedTimeline.domain[0], unfoldedTimeline.domain[1]]
+  )
+
+  renderer.setProps({
+    timecode: getTimecode(durationSec),
+    onTimeChange: function (timeMs) {
+      mapSdk.updateLayerTimeline({currentTime: getTimestamp(timeMs)})
+    }
+  })
 }
